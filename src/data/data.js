5379e2b68017c6ad9e8482f93ab1020dc1429ae1
Amogh Patel <amogh.patel.2000@gmail.com>
2023-08-09 04:07:14 +0530

    portfolio

---
+++ src/data/data.js
@@ -0,0 +1,38 @@
+import studenthub from '../assets/projects/StudentHub.png'
+import stockedge from '../assets/projects/StockEdge.png'
+import blogpost from '../assets/projects/blogpost.jpg'
+import weatherapp from '../assets/projects/weatherapp.jpg'
+import RealEst from '../assets/projects/realestate.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"StudentHub",
+        image:studenthub,
+        github:"https://github.com/AmoghPatel1/StudentHubClient",
+        live:"https://student-hub-wbd.netlify.app/login",
+    },
+    {
+        id:2,
+        name:"StockEdge",
+        image:stockedge,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"Blog Post Application",
+        image:blogpost,
+        github:"https://github.com/AmoghPatel1/Blog-Post-website",
+        live:"https://github.com/AmoghPatel1/Blog-Post-website",
+    },
+    {
+        id:4,
+        name:"Weather Application",
+        image:weatherapp,
+        github:"https://github.com/AmoghPatel1/Weather-App",
+        live:"https://github.com/AmoghPatel1/Weather-App",
+    },
+
+
+]
